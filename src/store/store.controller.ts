be31df11d3e7a26d5b4b3023198e0c4f7c7557e7
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-10-12 11:49:47 +0300

    Updated topics

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -3,32 +3,33 @@ import { StoreService } from './store.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateStoreDto } from './dto/create-store.dto';
 import { UpdateStoreDto } from './dto/update-store.dto';
+import { ListQuery, StoreTopics } from 'store-mag-types';
 
 @Controller('store')
 export class StoreController {
   constructor(private readonly service: StoreService) {}
 
-  @MessagePattern('store.list')
-  async list() {
+  @MessagePattern(StoreTopics.LIST_STORE)
+  async list(@Payload() query: ListQuery) {
     return await this.service.list();
   }
 
-  @MessagePattern('store.create')
+  @MessagePattern(StoreTopics.CREATE_STORE)
   async create(@Payload() data: CreateStoreDto) {
     return await this.service.create(data);
   }
 
-  @MessagePattern('store.update')
+  @MessagePattern(StoreTopics.UPDATE_STORE)
   async update(@Payload() data: UpdateStoreDto) {
     return await this.service.update(data);
   }
 
-  @MessagePattern('store.delete')
+  @MessagePattern(StoreTopics.DELETE_STORE)
   async delete(@Payload() id: string) {
     return await this.service.delete(id);
   }
 
-  @MessagePattern('store.findById')
+  @MessagePattern(StoreTopics.FIND_STORE)
   async findById(@Payload() id: string) {
     return await this.service.findById(id);
   }
